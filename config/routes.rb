31a8db2f55e4e64c2532590bdfe422c3c3334929
Mwaura Jr <jamhurifrankiline@gmail.com>
2024-06-03 23:10:25 +0300

    Redirect the users to verify code after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "home#index"
   devise_for :users
   get "/verify" => "verify#edit", :as => "verify"
   get "/verify" => "verify#new", :as => "new_verify"
   put "/verify" => "verify#update", :as => "update_verify"
   post "/verify" => "verify#create", :as => "resend_verify"
+  root "home#index"
 end
