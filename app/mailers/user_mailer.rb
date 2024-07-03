50b3d025c5280b1f815078491be88bc79c0e2a11
Mwaura Jr <jamhurifrankiline@gmail.com>
2024-07-04 00:28:47 +0300

    Final cleanup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def send_pin(user)
     @user = user
+    @user_name = user.email.split("@").first.capitalize
     mail(to: @user.email, subject: "Verification PIN Code")
   end
 end
